a9377ccfdc864a33973e3929826676b455effcd8
christainc3213 <christianc3213@gmail.com>
2025-03-28 12:06:07 -0600

    asdf

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,28 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import BookList from './components/BookList';
 import CartPage from './components/CartPage';
-import { useCart } from './context/CartContext';
 
 const App: React.FC = () => {
-    const { totalItems } = useCart();
-
     return (
         <Router>
-            <nav className="navbar navbar-light bg-light">
-                <Link to="/" className="navbar-brand">Bookstore</Link>
-                <Link to="/cart" className="btn btn-primary position-relative">
-                    View Cart
-                    {totalItems > 0 && (
-                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                            {totalItems}
-                        </span>
-                    )}
-                </Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<BookList />} />
-                <Route path="/cart" element={<CartPage />} />
-            </Routes>
+            <div className="container p-4">
+                <nav className="navbar navbar-light bg-light mb-4">
+                    <Link to="/" className="navbar-brand">Bookstore</Link>
+                    <Link to="/cart" className="btn btn-primary">View Cart</Link>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<BookList />} />
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </div>
         </Router>
     );
 };
